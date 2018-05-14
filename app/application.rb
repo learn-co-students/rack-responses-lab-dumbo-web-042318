cc4625e440f3f987027b1bcf17c485f554e516c5
snkoski <shawnnkoski@gmail.com>
2018-05-14 09:28:54 -0400

    Rack tells you good morning or good afternoon

---
+++ app/application.rb
@@ -0,0 +1,16 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+
+    time = Time.now
+
+    if time.hour >= 0 && time.hour < 12
+      resp.write "Morning"
+    else
+      resp.write "Afternoon"
+    end
+
+     resp.finish
+  end
+end
